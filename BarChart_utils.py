c946244bf012a50ebd667479a1fca5a3ea99dc55
Raghu Sanjeev <RaghuSanjeev69@gmail.com>
2020-11-16 15:04:41 +0530

    first commit

---
+++ BarChart_utils.py
@@ -0,0 +1,136 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+#https://matplotlib.org/gallery/lines_bars_and_markers/horizontal_barchart_distribution.html
+
+"""
+category_names = ['Strongly disagree', 'Disagree',
+                  'Neither agree nor disagree', 'Agree', 'Strongly agree']
+results = {
+    'Question 1': [10, 15, 17, 32, 26],
+    'Question 2': [26, 22, 29, 10, 13],
+    'Question 3': [35, 37, 7, 2, 19],
+    'Question 4': [32, 11, 9, 15, 33],
+    'Question 5': [21, 29, 5, 5, 40],
+    'Question 6': [8, 19, 5, 30, 38]
+}
+"""
+
+def horizontal_bar_chart(results, category_names):
+    """
+    Parameters
+    ----------
+    results : dict
+        A mapping from question labels to a list of answers per category.
+        It is assumed all lists contain the same number of entries and that
+        it matches the length of *category_names*.
+    category_names : list of str
+        The category labels.
+    """
+    labels = list(results.keys())
+    data = np.array(list(results.values()))
+    data_cum = data.cumsum(axis=1)
+    category_colors = plt.get_cmap('RdYlGn')(
+        np.linspace(0.15, 0.85, data.shape[1]))
+
+    fig, ax = plt.subplots(figsize=(9.2, 5))
+    ax.invert_yaxis()
+    ax.xaxis.set_visible(False)
+    ax.set_xlim(0, np.sum(data, axis=1).max())
+
+    for i, (colname, color) in enumerate(zip(category_names, category_colors)):
+        widths = data[:, i]
+        starts = data_cum[:, i] - widths
+        ax.barh(labels, widths, left=starts, height=0.5,
+                label=colname, color=color)
+        xcenters = starts + widths / 2
+
+        r, g, b, _ = color
+        text_color = 'white' if r * g * b < 0.5 else 'darkgrey'
+        for y, (x, c) in enumerate(zip(xcenters, widths)):
+            ax.text(x, y, str(int(c)), ha='center', va='center',
+                    color=text_color)
+    ax.legend(ncol=len(category_names), bbox_to_anchor=(0, 1),
+              loc='lower left', fontsize='small')
+
+    plt.show()
+    return fig, ax
+
+
+
+def plot_stacked_bar(data,
+                     series_labels,
+                     category_labels=None,
+                     show_values=False,
+                     value_format="{}",
+                     x_label=None,
+                     y_label=None,
+                     colors=None,
+                     grid=True,
+                     reverse=False):
+    """Plots a stacked bar chart with the data and labels provided.
+
+    Keyword arguments:
+    data            -- 2-dimensional numpy array or nested list
+                       containing data for each series in rows
+    series_labels   -- list of series labels (these appear in
+                       the legend)
+    category_labels -- list of category labels (these appear
+                       on the x-axis)
+    show_values     -- If True then numeric value labels will
+                       be shown on each bar
+    value_format    -- Format string for numeric value labels
+                       (default is "{}")
+    y_label         -- Label for y-axis (str)
+    colors          -- List of color labels
+    grid            -- If True display grid
+    reverse         -- If True reverse the order that the
+                       series are displayed (left-to-right
+                       or right-to-left)
+    """
+
+    plt.figure(figsize=(6, 4))
+
+    ny = len(data[0])
+    ind = list(range(ny))
+
+    axes = []
+    cum_size = np.zeros(ny)
+
+    data = np.array(data)
+
+    if reverse:
+        data = np.flip(data, axis=1)
+        category_labels = reversed(category_labels)
+
+    for i, row_data in enumerate(data):
+        color = colors[i] if colors is not None else None
+        axes.append(plt.bar(ind, row_data, bottom=cum_size,
+                            label=series_labels[i], color=color))
+        cum_size += row_data
+
+    if category_labels:
+        plt.xticks(ind, category_labels)
+
+    if y_label:
+        plt.ylabel(y_label)
+
+    if x_label:
+        plt.xlabel(x_label)
+
+    plt.legend()
+
+    if grid:
+        plt.grid()
+
+    if show_values:
+        for axis in axes:
+            for bar in axis:
+                w, h = bar.get_width(), bar.get_height()
+                plt.text(bar.get_x() + w/2,
+                         bar.get_y() + h/2,
+                         int(float(value_format.format(h))),
+                         ha="center",
+                         va="center")
+    plt.tight_layout()
+    plt.show()
